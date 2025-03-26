f9bdc43d7941a3a37db0edf683ce057031d2ee50
andrea.ruggerini.soource <andrea.ruggerini@soource.com>
2025-03-26 23:02:00 +0100

    fasthtml example

--- main.py
+++ main.py
@@ -1,3 +1,4 @@
+from fasthtml.common import Div, P, FastHTML
 from fastapi import FastAPI
 
 app = FastAPI()
@@ -7,13 +8,16 @@ app = FastAPI()
 def read_main():
     return {"message": "Hello World from main app"}
 
-
+'''
 subapi = FastAPI()
-
-
 @subapi.get("/sub")
 def read_sub():
     return {"message": "Hello World from sub API"}
+'''
 
 
-app.mount("/subapi", subapi)
+app2 = FastHTML()
+@app2.get('/x')
+def get(): return Div(P('Hello World!'), hx_get="/change")
+
+app.mount("/subapi", app2)
